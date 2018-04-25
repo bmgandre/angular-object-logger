4b189822ce99a588b2fd02a6a51ddad5ef940b3e
André Gomes <bmg.andre@gmail.com>
2018-04-24 22:22:39 -0300

    [test] fix test describing

--- src/module/service/config/filter/multi-level-log-filter-parser.spec.ts
+++ src/module/service/config/filter/multi-level-log-filter-parser.spec.ts
@@ -4,166 +4,169 @@ import { LogLevel } from '../../log-level.model';
 
 describe('Parse multi-level filters', () => {
 
-    describe('given a configuration containing [ info, warn, error ] filters', () => {
+    describe('given a configuration containing a multi-level filter', () => {
 
-        let multiLevelLogFilterParser: MultiLevelLogFilterParser;
-        let filter: MultiLevelLogFilter;
-        const environment = {
-            levels: [ 'info', 'warn', 'error' ]
-        };
+        describe('when the parser is called with [ info, warn, error ] filters', () => {
 
-        beforeAll(() => {
-            multiLevelLogFilterParser = new MultiLevelLogFilterParser();
-            filter = multiLevelLogFilterParser.parse(environment);
+            let multiLevelLogFilterParser: MultiLevelLogFilterParser;
+            let filter: MultiLevelLogFilter;
+            const environment = {
+                levels: [ 'info', 'warn', 'error' ]
+            };
+
+            beforeAll(() => {
+                multiLevelLogFilterParser = new MultiLevelLogFilterParser();
+                filter = multiLevelLogFilterParser.parse(environment);
+            });
+
+            test('then the filter should be crated', () => {
+                expect(filter).not
+                    .toBeNull();
+            });
+
+            test('then the filter loglevel should reject from trace level', () => {
+                expect(filter.IsLevelMatch(LogLevel.trace))
+                    .toBeFalsy();
+            });
+
+            test('then the filter loglevel should reject from debug level', () => {
+                expect(filter.IsLevelMatch(LogLevel.debug))
+                    .toBeFalsy();
+            });
+
+            test('then the filter loglevel should accept from info level', () => {
+                expect(filter.IsLevelMatch(LogLevel.info))
+                    .toBeTruthy();
+            });
+
+            test('then the filter loglevel should accept from warn level', () => {
+                expect(filter.IsLevelMatch(LogLevel.warn))
+                    .toBeTruthy();
+            });
+
+            test('then the filter loglevel should accept from error level', () => {
+                expect(filter.IsLevelMatch(LogLevel.error))
+                    .toBeTruthy();
+            });
+
+            test('then the filter loglevel should reject from fatal level', () => {
+                expect(filter.IsLevelMatch(LogLevel.fatal))
+                    .toBeFalsy();
+            });
+
+            test('then the source should be .*', () => {
+                expect(filter.source)
+                    .toBe('.*');
+            });
         });
 
-        test('then the filter should be crated', () => {
-            expect(filter).not
-                .toBeNull();
+        describe('when the parser is called with [ trace, fatal, error ] filters and HomeComponent source', () => {
+
+            let multiLevelLogFilterParser: MultiLevelLogFilterParser;
+            let filter: MultiLevelLogFilter;
+            const environment = {
+                levels: [ 'trace', 'fatal', 'error' ],
+                source: 'HomeComponent'
+            };
+
+            beforeAll(() => {
+                multiLevelLogFilterParser = new MultiLevelLogFilterParser();
+                filter = multiLevelLogFilterParser.parse(environment);
+            });
+
+            test('then the filter should be crated', () => {
+                expect(filter).not
+                    .toBeNull();
+            });
+
+            test('then the filter loglevel should accept from trace level', () => {
+                expect(filter.IsLevelMatch(LogLevel.trace))
+                    .toBeTruthy();
+            });
+
+            test('then the filter loglevel should reject from debug level', () => {
+                expect(filter.IsLevelMatch(LogLevel.debug))
+                    .toBeFalsy();
+            });
+
+            test('then the filter loglevel should reject from info level', () => {
+                expect(filter.IsLevelMatch(LogLevel.info))
+                    .toBeFalsy();
+            });
+
+            test('then the filter loglevel should reject from warn level', () => {
+                expect(filter.IsLevelMatch(LogLevel.warn))
+                    .toBeFalsy();
+            });
+
+            test('then the filter loglevel should accept from error level', () => {
+                expect(filter.IsLevelMatch(LogLevel.error))
+                    .toBeTruthy();
+            });
+
+            test('then the filter loglevel should accept from fatal level', () => {
+                expect(filter.IsLevelMatch(LogLevel.fatal))
+                    .toBeTruthy();
+            });
+
+            test('then the source should be HomeComponent', () => {
+                expect(filter.source)
+                    .toBe('HomeComponent');
+            });
         });
 
-        test('then the filter loglevel should reject from trace level', () => {
-            expect(filter.IsLevelMatch(LogLevel.trace))
-                .toBeFalsy();
-        });
+        describe('when the parser is called with [ debug ] filters', () => {
 
-        test('then the filter loglevel should reject from debug level', () => {
-            expect(filter.IsLevelMatch(LogLevel.debug))
-                .toBeFalsy();
-        });
+            let multiLevelLogFilterParser: MultiLevelLogFilterParser;
+            let filter: MultiLevelLogFilter;
+            const environment = {
+                levels: [ 'debug' ]
+            };
 
-        test('then the filter loglevel should accept from info level', () => {
-            expect(filter.IsLevelMatch(LogLevel.info))
-                .toBeTruthy();
-        });
+            beforeAll(() => {
+                multiLevelLogFilterParser = new MultiLevelLogFilterParser();
+                filter = multiLevelLogFilterParser.parse(environment);
+            });
 
-        test('then the filter loglevel should accept from warn level', () => {
-            expect(filter.IsLevelMatch(LogLevel.warn))
-                .toBeTruthy();
-        });
+            test('then the filter should be crated', () => {
+                expect(filter).not
+                    .toBeNull();
+            });
 
-        test('then the filter loglevel should accept from error level', () => {
-            expect(filter.IsLevelMatch(LogLevel.error))
-                .toBeTruthy();
-        });
+            test('then the filter loglevel should reject from trace level', () => {
+                expect(filter.IsLevelMatch(LogLevel.trace))
+                    .toBeFalsy();
+            });
 
-        test('then the filter loglevel should reject from fatal level', () => {
-            expect(filter.IsLevelMatch(LogLevel.fatal))
-                .toBeFalsy();
-        });
+            test('then the filter loglevel should accept from debug level', () => {
+                expect(filter.IsLevelMatch(LogLevel.debug))
+                    .toBeTruthy();
+            });
 
-        test('then the source should be .*', () => {
-            expect(filter.source)
-                .toBe('.*');
-        });
-    });
+            test('then the filter loglevel should reject from info level', () => {
+                expect(filter.IsLevelMatch(LogLevel.info))
+                    .toBeFalsy();
+            });
 
-    describe('given a configuration containing [ trace, fatal, error ] filters and HomeComponent source', () => {
+            test('then the filter loglevel should reject from warn level', () => {
+                expect(filter.IsLevelMatch(LogLevel.warn))
+                    .toBeFalsy();
+            });
 
-        let multiLevelLogFilterParser: MultiLevelLogFilterParser;
-        let filter: MultiLevelLogFilter;
-        const environment = {
-            levels: [ 'trace', 'fatal', 'error' ],
-            source: 'HomeComponent'
-        };
+            test('then the filter loglevel should reject from error level', () => {
+                expect(filter.IsLevelMatch(LogLevel.error))
+                    .toBeFalsy();
+            });
 
-        beforeAll(() => {
-            multiLevelLogFilterParser = new MultiLevelLogFilterParser();
-            filter = multiLevelLogFilterParser.parse(environment);
-        });
+            test('then the filter loglevel should reject from fatal level', () => {
+                expect(filter.IsLevelMatch(LogLevel.fatal))
+                    .toBeFalsy();
+            });
 
-        test('then the filter should be crated', () => {
-            expect(filter).not
-                .toBeNull();
-        });
-
-        test('then the filter loglevel should accept from trace level', () => {
-            expect(filter.IsLevelMatch(LogLevel.trace))
-                .toBeTruthy();
-        });
-
-        test('then the filter loglevel should reject from debug level', () => {
-            expect(filter.IsLevelMatch(LogLevel.debug))
-                .toBeFalsy();
-        });
-
-        test('then the filter loglevel should reject from info level', () => {
-            expect(filter.IsLevelMatch(LogLevel.info))
-                .toBeFalsy();
-        });
-
-        test('then the filter loglevel should reject from warn level', () => {
-            expect(filter.IsLevelMatch(LogLevel.warn))
-                .toBeFalsy();
-        });
-
-        test('then the filter loglevel should accept from error level', () => {
-            expect(filter.IsLevelMatch(LogLevel.error))
-                .toBeTruthy();
-        });
-
-        test('then the filter loglevel should accept from fatal level', () => {
-            expect(filter.IsLevelMatch(LogLevel.fatal))
-                .toBeTruthy();
-        });
-
-        test('then the source should be HomeComponent', () => {
-            expect(filter.source)
-                .toBe('HomeComponent');
-        });
-    });
-
-    describe('given a configuration containing [ debug ] filters', () => {
-
-        let multiLevelLogFilterParser: MultiLevelLogFilterParser;
-        let filter: MultiLevelLogFilter;
-        const environment = {
-            levels: [ 'debug' ]
-        };
-
-        beforeAll(() => {
-            multiLevelLogFilterParser = new MultiLevelLogFilterParser();
-            filter = multiLevelLogFilterParser.parse(environment);
-        });
-
-        test('then the filter should be crated', () => {
-            expect(filter).not
-                .toBeNull();
-        });
-
-        test('then the filter loglevel should reject from trace level', () => {
-            expect(filter.IsLevelMatch(LogLevel.trace))
-                .toBeFalsy();
-        });
-
-        test('then the filter loglevel should accept from debug level', () => {
-            expect(filter.IsLevelMatch(LogLevel.debug))
-                .toBeTruthy();
-        });
-
-        test('then the filter loglevel should reject from info level', () => {
-            expect(filter.IsLevelMatch(LogLevel.info))
-                .toBeFalsy();
-        });
-
-        test('then the filter loglevel should reject from warn level', () => {
-            expect(filter.IsLevelMatch(LogLevel.warn))
-                .toBeFalsy();
-        });
-
-        test('then the filter loglevel should reject from error level', () => {
-            expect(filter.IsLevelMatch(LogLevel.error))
-                .toBeFalsy();
-        });
-
-        test('then the filter loglevel should reject from fatal level', () => {
-            expect(filter.IsLevelMatch(LogLevel.fatal))
-                .toBeFalsy();
-        });
-
-        test('then the source should be .*', () => {
-            expect(filter.source)
-                .toBe('.*');
+            test('then the source should be .*', () => {
+                expect(filter.source)
+                    .toBe('.*');
+            });
         });
     });
 });
